7a7da31ab2756e96e2740f14da7575a46746b421
Ibrahim Asiri <i-m-6@hotmail.com>
2021-11-13 17:49:18 +0300

    Created a project file and link it with Firebase and add libraries

---
+++ JetChat/JetChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JetChat
+//
+//  Created by ibrahim asiri on 08/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
