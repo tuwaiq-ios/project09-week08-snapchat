72c3b7c09d4c9cf22bca9e0567eb0794bd3710f9
dmdm <deemamoh15@hotmail.com>
2021-11-15 21:34:50 +0300

    i added app icon and delete function and contacts list

---
+++ SnnapChatApp/SnnapChatApp/ContactListVC.swift
@@ -0,0 +1,13 @@
+//
+//  ContactListVC.swift
+//  SnnapChatApp
+//
+//  Created by dmdm on 15/11/2021.
+//
+
+import UIKit
+
+class ContactListVC: UIViewController {
+    
+    
+}
--- SnnapChatApp/SnnapChatApp/TabVC.swift
+++ SnnapChatApp/SnnapChatApp/TabVC.swift
@@ -29,18 +29,22 @@ import Firebase
     let item2 = ProfileVC()
     let item3 = LocationVC()
 //      let item4 = CamVC()
+    let item5 = ContactListVC()
+      
     let icon1 = UITabBarItem(title: "chat", image: UIImage(systemName: "contextualmenu.and.cursorarrow"), selectedImage: UIImage(systemName: "contextualmenu.and.cursorarrow"))
       
     let icon2 = UITabBarItem(title: "profile", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person"))
       
       let icon3 = UITabBarItem(title: "Location", image: UIImage(systemName: "location"), selectedImage: UIImage(systemName: "location"))
 //      let icon4 = UITabBarItem(title: "Camera", image: UIImage(systemName: "camera"), selectedImage: UIImage(systemName: "camera"))
+      let icon5 = UITabBarItem(title: "Contacts", image: UIImage(systemName: "person.3.fill"), selectedImage: UIImage(systemName: "person.3.fill"))
       
     item1.tabBarItem = icon1
     item2.tabBarItem = icon2
     item3.tabBarItem = icon3
 //    item4.tabBarItem = icon4
-    let controllers = [item1,item2,item3] //array of the root view controllers displayed by the tab bar interface
+      item5.tabBarItem = icon5
+    let controllers = [item1,item2,item3,item5] //array of the root view controllers displayed by the tab bar interface
     self.viewControllers = controllers
   }
   func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
