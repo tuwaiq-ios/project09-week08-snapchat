f981c30131d4936d0a884a6e8507e5d2f2d2fb09
Sana Alshahrani <me-sa51@hotmail.com>
2021-11-15 16:54:54 +0300

    created Chat list with SearchBar

---
+++ JetChat/JetChat/Chat/DMScreen.swift
@@ -0,0 +1,21 @@
+//
+//  DMScreen.swift
+//  JetChat
+//
+//  Created by Sana Alshahrani on 10/04/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class DMScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+}
+    
+}
+
+
