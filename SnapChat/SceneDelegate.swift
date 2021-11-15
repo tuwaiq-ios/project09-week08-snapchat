a5cea6b9806be2a13e34eee00d553aaa58724de4
sara saud <malik@Abdulmaliks-MacBook-Pro.local>
2021-11-15 16:50:12 +0300

    TikTokVC, constreants

--- SnapChat/SceneDelegate.swift
+++ SnapChat/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
     let navigationController = UINavigationController(rootViewController: RegisterVC())
-        let home = CamVC()
+        let home = LoginOrRegisterVC()
         //  let home = LoginOrRegisterVC()
         self.window?.rootViewController = home
         window?.makeKeyAndVisible()
