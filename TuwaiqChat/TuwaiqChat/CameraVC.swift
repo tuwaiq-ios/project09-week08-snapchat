cff6e74c6bb637b166743a852c28fd305f3b15aa
maram <maarram36@gmail.com>
2021-11-16 11:27:49 +0300

    created auser profile and TabBarVC

---
+++ TuwaiqChat/TuwaiqChat/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  TuwaiqChat
+//
+//  Created by Maram Al shahrani on 11/04/1443 AH.
+//
+
+import UIKit
+
+
+class CameraVC: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+    
+}
---
+++ TuwaiqChat/TuwaiqChat/MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  MapVC.swift
+//  TuwaiqChat
+//
+//  Created by Maram Al shahrani on 11/04/1443 AH.
+//
+
+import UIKit
+
+class MapVC : UIViewController {
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+    
+}
