15dbdca8f46e53a1423d6ceeba320c77bd3ea400
sallyasiri7 <sallywork2022@gmail.com>
2021-11-19 22:03:03 +0300

    Edit in mapVC

--- snapchatt/snapchatt/snapchatt/Controllers/MapVC.swift
+++ snapchatt/snapchatt/snapchatt/Controllers/MapVC.swift
@@ -4,57 +4,37 @@
 //
 //  Created by sally asiri on 08/04/1443 AH.
 //
-
 import UIKit
 import MapKit
-import CoreLocation
-import Firebase
-import FirebaseFirestore
 
-class MapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-	var locationManager: CLLocationManager!
-	var locationManger: CLLocationManager!
-	lazy var mapView: MKMapView = {
-		let mv = MKMapView()
-		mv.translatesAutoresizingMaskIntoConstraints = false
-		return mv
-	}()
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		mapView.delegate = self
-		if CLLocationManager.locationServicesEnabled() {
-			locationManger = CLLocationManager()
-			locationManger.delegate = self
-			locationManger.desiredAccuracy = kCLLocationAccuracyBest
-			locationManger.requestAlwaysAuthorization()
-			locationManger.startUpdatingLocation()
-		}
-		view .addSubview(mapView)
-		NSLayoutConstraint.activate([
-			mapView.leftAnchor.constraint(equalTo: view.leftAnchor),
-			mapView.rightAnchor.constraint(equalTo: view.rightAnchor),
-			mapView.topAnchor.constraint(equalTo: view.topAnchor),
-			mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-		])
-//		let users: Array <Any> = []
-//		for user in users {
-//			let userPin = MKPointAnnotation()
-//			userPin.title = (user as AnyObject).name
-//			userPin.coordinate = CLLocationCoordinate2D(
-//				latitude: users.lat, longitude: users.long
-//			)
-//			mapView.addAnnotation(userPin)
-//		}
-	}
-}
-func locationManager(_ manager: CLLocationManager,
-					 didUpdateLocations location: [CLLocation]) {
-	guard let lastLocation = location.last else { return }
-	let lat = lastLocation.coordinate.latitude
-	let long = lastLocation.coordinate.longitude
-	guard let currentUserId = Auth.auth().currentUser?.uid else { return }
-	Firestore.firestore().collection("users").document(currentUserId).setData([
-		"lat": lat,
-		"long": long]
-		, merge: true)
+class MapVC: UIViewController {
+  let mapView : MKMapView = {
+    let map = MKMapView()
+    map.overrideUserInterfaceStyle = .light
+    return map
+  }()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setMapConstraints()
+  }
+  func setMapConstraints() {
+    view.addSubview(mapView)
+    mapView.translatesAutoresizingMaskIntoConstraints = false
+    mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+    mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+    mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    let A = MKPointAnnotation()
+    A.title = "sally"
+    A.coordinate = CLLocationCoordinate2D(latitude: 24.5246542, longitude: 39.5691841)
+    mapView.addAnnotation(A)
+    let S = MKPointAnnotation()
+    S.title = "tasneem"
+    S.coordinate = CLLocationCoordinate2D(latitude: 24.7135517, longitude: 46.6752957)
+    mapView.addAnnotation(S)
+    let F = MKPointAnnotation()
+    F.title = "Hasan"
+    F.coordinate = CLLocationCoordinate2D(latitude: 18.084764, longitude: 43.138569)
+    mapView.addAnnotation(F)
+  }
 }
