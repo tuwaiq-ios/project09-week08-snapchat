f03ff41e444d16ccb217916dd2938f39d0ca7736
sara al zhrani <saraalzhrani@MacBook-Pro-alkhas-b-sara.local>
2021-11-20 23:51:29 +0300

    camera replaced

---
+++ SnapChatGroupC/SnapChatGroupC/New Group/CameraVC.swift
@@ -0,0 +1,199 @@
+//
+//  CameraVC.swift
+//  SnapChatGroupC
+//
+//  Created by sara al zhrani on 15/04/1443 AH.
+//
+
+
+import AVFoundation
+import UIKit
+
+
+class CamVC : UIViewController {
+    
+  var captureSession : AVCaptureSession!
+    
+    var backCamera : AVCaptureDevice!
+    var frontCamera : AVCaptureDevice!
+    var backInput : AVCaptureInput!
+    var frontInput : AVCaptureInput!
+    
+    var previewLayer : AVCaptureVideoPreviewLayer!
+    
+    var videoOutput : AVCaptureVideoDataOutput!
+    
+    var takePicture = false
+    var backCameraOn = true
+    
+    let switchCameraButton : UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "switchcamera")?.withRenderingMode(.alwaysTemplate)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let captureImageButton : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.tintColor = .white
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let capturedImageView = CapturedImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkPermissions()
+        setupAndStartCaptureSession()
+    }
+    
+    func setupAndStartCaptureSession(){
+        DispatchQueue.global(qos: .userInitiated).async{
+            //init session
+            self.captureSession = AVCaptureSession()
+            self.captureSession.beginConfiguration()
+            
+            if self.captureSession.canSetSessionPreset(.photo) {
+                self.captureSession.sessionPreset = .photo
+            }
+            self.captureSession.automaticallyConfiguresCaptureDeviceForWideColor = true
+            
+          
+            self.setupInputs()
+            
+            DispatchQueue.main.async {
+                self.setupPreviewLayer()
+            }
+            
+            self.setupOutput()
+            
+            self.captureSession.commitConfiguration()
+            self.captureSession.startRunning()
+        }
+    }
+    
+    func setupInputs(){
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            backCamera = device
+        } else {
+            fatalError("no back camera")
+        }
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+            frontCamera = device
+        } else {
+            fatalError("no front camera")
+        }
+        
+        guard let bInput = try? AVCaptureDeviceInput(device: backCamera) else {
+            fatalError("could not create input device from back camera")
+        }
+        backInput = bInput
+        if !captureSession.canAddInput(backInput) {
+            fatalError("could not add back camera input to capture session")
+        }
+        
+        guard let fInput = try? AVCaptureDeviceInput(device: frontCamera) else {
+            fatalError("could not create input device from front camera")
+        }
+        frontInput = fInput
+        if !captureSession.canAddInput(frontInput) {
+            fatalError("could not add front camera input to capture session")
+        }
+        
+        captureSession.addInput(backInput)
+    }
+    
+    func setupOutput(){
+        videoOutput = AVCaptureVideoDataOutput()
+        let videoQueue = DispatchQueue(label: "videoQueue", qos: .userInteractive)
+        videoOutput.setSampleBufferDelegate(self, queue: videoQueue)
+        
+        if captureSession.canAddOutput(videoOutput) {
+            captureSession.addOutput(videoOutput)
+        } else {
+            fatalError("could not add video output")
+        }
+        
+        videoOutput.connections.first?.videoOrientation = .portrait
+    }
+    
+    func setupPreviewLayer(){
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.insertSublayer(previewLayer, below: switchCameraButton.layer)
+        previewLayer.frame = self.view.layer.frame
+    }
+    
+    func switchCameraInput(){
+        //don't let user spam the button, fun for the user, not fun for performance
+        switchCameraButton.isUserInteractionEnabled = false
+        
+        //reconfigure the input
+        captureSession.beginConfiguration()
+        if backCameraOn {
+            captureSession.removeInput(backInput)
+            captureSession.addInput(frontInput)
+            backCameraOn = false
+        } else {
+            captureSession.removeInput(frontInput)
+            captureSession.addInput(backInput)
+            backCameraOn = true
+        }
+        
+        //deal with the connection again for portrait mode
+        videoOutput.connections.first?.videoOrientation = .portrait
+        
+        //mirror the video stream for front camera
+        videoOutput.connections.first?.isVideoMirrored = !backCameraOn
+        
+        //commit config
+        captureSession.commitConfiguration()
+        
+        //acitvate the camera button again
+        switchCameraButton.isUserInteractionEnabled = true
+    }
+    
+    //MARK:- Actions
+    @objc func captureImage(_ sender: UIButton?){
+        takePicture = true
+    }
+    
+    @objc func switchCamera(_ sender: UIButton?){
+        switchCameraInput()
+    }
+
+}
+
+extension CamVC : AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if !takePicture {
+            return
+        }
+        
+       
+        guard let cvBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let ciImage = CIImage(cvImageBuffer: cvBuffer)
+        
+        let uiImage = UIImage(ciImage: ciImage)
+        
+        DispatchQueue.main.async {
+            self.capturedImageView.image = uiImage
+            self.takePicture = false
+        }
+    }
+        
+}
